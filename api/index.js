24837538f8f69fe57d048a3eac4d8ceee83afe82
ThiagoScholte <93194632+ThiagoScholte@users.noreply.github.com>
2023-02-06 11:30:20 +0000

    rating

--- api/index.js
+++ api/index.js
@@ -67,7 +67,7 @@ function getCategories(request, response) {
 function getProducts(request, response) {
   console.log('API ontvangt /api/products/', request.query)
   let data = []
-  const sqlOpdracht = db.prepare('SELECT products.id AS id, products.name AS name, products.description AS description, products.code AS code, products.price AS price FROM products ORDER BY products.name ASC')
+  const sqlOpdracht = db.prepare('SELECT products.id AS id, products.name AS name, products.description AS description, products.code AS code, products.price AS price, products.Rating_Product AS rating FROM products ORDER BY Rating_Product ASC')
   data = sqlOpdracht.all()
   // console.log(JSON.stringify(data, null, 2))
   response.status(200).send(data)
@@ -78,7 +78,7 @@ function getProductById(request, response) {
   console.log('API ontvangt /api/products/:id', request.query)
   let data = []
   const product_id = parseInt(request.params.id)
-  const sqlOpdracht = db.prepare('SELECT products.id AS id, products.name AS name, products.description AS description, products.code AS code, products.price AS price FROM products WHERE id = ?')
+  const sqlOpdracht = db.prepare('SELECT products.id AS id, products.name AS name, products.description AS description, products.code AS code, products.Rating_Product AS rating, products.price AS price FROM products WHERE id = ?')
   data = sqlOpdracht.all(product_id)
   response.status(200).json(data[0])
 }
--- db/create.sql
+++ db/create.sql
@@ -7,16 +7,11 @@ CREATE TABLE products (
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
-  price NUMERIC(10, 2)
+  price NUMERIC(10, 2),
+  Category_ID NUMERIC (2),
+  Rating_Product NUMERIC (2,1)
 );
 
-CREATE TABLE informatie_product (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  information TEXT,
-  rating NUMERIC(2,2),
-  price TEXT
-  id VARCHAR(1),
-);
 
 --
 -- populate with data
@@ -26,12 +21,14 @@ CREATE TABLE informatie_product (
 --
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
-insert into informatie_product (information, rating, price) values ('dit is informatie', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0');
 
-insert into products (name, description, code, price) values ('film', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('Post Mortem', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('Scarlet and the Black, The', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
-
+insert into products (name, description, code, price, Rating_Product) values ('Product 1', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5, 5);
+insert into products (name, description, code, price, Rating_Product) values ('Product 2', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11.50, 3);
+insert into products (name, description, code, price, Rating_Product) values ('Product 3', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5, 4);
+insert into products (name, description, code, price, Rating_Product) values ('Product 4', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5, 2);
+insert into products (name, description, code, price, Rating_Product) values ('Product 5', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14, 4);
+insert into products (name, description, code, price, Rating_Product) values ('Product 6', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14, 2);
+insert into products (name, description, code, price, Rating_Product) values ('Product 7', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14, 3);
+insert into products (name, description, code, price, Rating_Product) values ('Product 8', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14, 3.5);
+insert into products (name, description, code, price, Rating_Product) values ('Product 9', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14, 2.2);
+insert into products (name, description, code, price, Rating_Product) values ('Product 10', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14, 3.5);
