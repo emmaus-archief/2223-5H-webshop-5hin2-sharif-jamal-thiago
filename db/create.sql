9168f76261a172df43ba5be0f7d6873db105e6fa
sharifk <74650500+sharifkondu@users.noreply.github.com>
2023-02-19 20:35:10 +0000

    laatste 3 producten toegevoegd, nog niet af

--- db/create.sql
+++ db/create.sql
@@ -55,15 +55,31 @@ CREATE TABLE categorie (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 
     
-insert into products (name, description, code, price, Rating_Product) values ('RTX 4090', 'rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 1959.00, 5);
-insert into products (name, description, code, price, Rating_Product) values ('RTX 4080', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 1489.00, 3);
-insert into products (name, description, code, price, Rating_Product) values ('AMD Ryzen 7 5600X 3D', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 330.00, 4);
-insert into products (name, description, code, price, Rating_Product) values ('AMD Ryzen 5 5600X', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 190.50, 2);
-insert into products (name, description, code, price, Rating_Product) values ('ASUS ROG STRIX B650E-F GAMING WIFI ', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14, 4);
-insert into products (name, description, code, price, Rating_Product) values ('Asus ROG STRIX B550-F GAMING', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14, 2);
-insert into products (name, description, code, price, Rating_Product) values ('Product 7', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14, 3);
-insert into products (name, description, code, price, Rating_Product) values ('Product 8', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14, 3.5);
-insert into products (name, description, code, price, Rating_Product) values ('Product 10', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14, 3.5);
+insert into products (name, description, code, price, Rating_Product) values ('RTX 4090', '  ', '816905633-0', 1959.00, 5);
+
+
+insert into products (name, description, code, price, Rating_Product) values ('RTX 4080', ' ', '077030122-3', 1489.00, 3);
+
+
+insert into products (name, description, code, price, Rating_Product) values ('AMD Ryzen 7 5600X 3D', ' ', '445924201-X', 330.00, 4);
+
+
+insert into products (name, description, code, price, Rating_Product) values ('AMD Ryzen 5 5600X', '  ', '693155505-7', 190.50, 2);
+
+
+insert into products (name, description, code, price, Rating_Product) values ('ASUS ROG STRIX B650E-F GAMING WIFI ', ' ', '686928463-6', 14, 4);
+
+
+insert into products (name, description, code, price, Rating_Product) values ('Asus ROG STRIX B550-F GAMING', '  ', '492662523-7', 14, 2);
+
+
+insert into products (name, description, code, price, Rating_Product) values ('Asrock Intel Arc A770', '  ', '492662523-7', 14, 3);
+
+insert into products (name, description, code, price, Rating_Product) values ('Asrock Intel Arc A380', ' ', '492662523-7', 14, 3.5);
+
+
+insert into products (name, description, code, price, Rating_Product) values ('Gigabyte Z790 AORUS ELITE AX', '  ', '492662523-7', 14, 3.5);
+
 
 insert into ratings (name) values ('3 van de 5 sterren');
 insert into ratings (name) values ('4 van de 5 sterren');
